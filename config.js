21766acd9fe6323227c8c7947ae1117a432cd696
jircdeveloper <jircdev@gmail.com>
2022-07-27 11:09:59 -0300

    delete

--- config.js
+++
@@ -1,4 +0,0 @@
-/************************
-APPLICATION CONFIGURATION
-************************/
-globalThis.__beyond_config = {"distribution":{"local":true,"key":"1334341767","environment":"development","mode":"amd","baseDir":"/"},"application":{"package":"@beyond/docs","version":1,"languages":{"default":"en","supported":["en","es"]},"params":{}},"backends":[["@beyond-js/local/legacy","http://localhost:3034"],["@beyond/docs/backend","http://localhost:3033"]]};
